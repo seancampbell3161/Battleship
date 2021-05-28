0c53a31dbc8bcc5c76de8553e224a20400e3fe9a
Sean Campbell <sean.campbell3161@gmail.com>
2021-05-28 16:45:27 -0500

    add method to set up game, play, check guess, and finish

--- src/GameTest.java
+++ src/GameTest.java
@@ -1,23 +1,69 @@
+import java.util.ArrayList;
+
 public class GameTest {
 
-    public static void main(String[] args) {
-        BattleshipGame game = new BattleshipGame();
-        GameHelper help = new GameHelper();
-        int numOfGuesses = 0;
-        int randomNum = (int) (Math.random() * 5);
-        int[] locations = {randomNum, randomNum+1, randomNum+2};
+    private GameHelper helper = new GameHelper();
+    private ArrayList<BattleshipGame> shipList = new ArrayList<>();
+    private int numOfGuesses = 0;
 
-        game.setLocationCells(locations);
-        boolean isAlive = true;
+    public void setUpGame() {
+        BattleshipGame ship1 = new BattleshipGame();
+        ship1.setName("Red October");
+        BattleshipGame ship2 = new BattleshipGame();
+        ship2.setName("The Black Pearl")
+        BattleshipGame ship3 = new BattleshipGame();
+        ship3.setName("Davey Jones");
+        shipList.add(ship1);
+        shipList.add(ship2);
+        shipList.add(ship3);
 
-        while(isAlive) {
-            String guess = help.getUserInput("enter a number");
-            String result = game.checkYourself(guess);
-            numOfGuesses++;
+        System.out.println("Your goal is to sink three ships:");
+        System.out.println("Red October, The Black Pearl, and Davey Jones");
+        System.out.println("Try to sink them all in the fewest number of guesses");
+
+        for(BattleshipGame ship : shipList) {
+            ArrayList<String> newLocation = helper.placeShip();
+            shipToSet.setLocationCells(newLocation);
+        }
+
+    }
+
+    public void startPlaying() {
+        while(!shipList.isEmpty()) {
+            String userGuess = helper.getUserInput("Enter a guess");
+            checkUserGuess(userGuess);
+        }
+        finishGame();
+    }
+
+    public void checkUserGuess(String userGuess) {
+        numOfGuesses++;
+        String result = "miss";
+        for(BattleshipGame ship : shipList) {
+            result = ship.checkYourself(userGuess);
+            if(result.equals("hit")) {
+                break;
+            }
             if(result.equals("kill")) {
-                isAlive = false;
-                System.out.println("You took " + numOfGuesses + " guesses");
+                shipList.remove(ship);
+                break;
             }
         }
+        System.out.println(result);
+    }
+
+    public void finishGame() {
+        System.out.println("Game Over");
+        if(numOfGuesses <= 18) {
+            System.out.println("Congratulations! You did well.");
+        } else {
+            System.out.println("You didn't do so hot, there.");
+        }
+    }
+
+    public static void main(String[] args) {
+        GameTest game = new GameTest();
+        game.setUpGame();
+        game.startPlaying();
     }
 }
