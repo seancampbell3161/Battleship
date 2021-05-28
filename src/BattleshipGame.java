2c53d09db494062ecb14ffdd1a5566a872fccda6
Sean Campbell <sean.campbell3161@gmail.com>
2021-05-28 16:57:22 -0500

    add functionality for 3 ships

--- src/BattleshipGame.java
+++ src/BattleshipGame.java
@@ -1,26 +1,27 @@
+import java.util.ArrayList;
+
 public class BattleshipGame {
 
-    int[] locationCells;
-    int numOfHits = 0;
+    private ArrayList<String> locationCells;
+    private String name;
 
-    public void setLocationCells(int[] locs) {
-        locationCells = locs;
+    public void setLocationCells(ArrayList<Integer> loc) {
+        locationCells = loc;
     }
+    public void setName(String name) {this.name = name;}
 
-    public String checkYourself(String stringGuess) {
-        int guess = Integer.parseInt(stringGuess);
+    public String checkYourself(String userInput) {
         String result = "miss";
-        for(int cell : locationCells) {
-            if(guess == cell) {
+        int index = locationCells.indexOf(userInput);
+        if(index >= 0) {
+            locationCells.remove(index);
+            if(locationCells.isEmpty()) {
+                result = "kill";
+                System.out.println("Ouch! You sunk " + name + "!");
+            } else {
                 result = "hit";
-                numOfHits++;
-                break;
             }
         }
-        if(numOfHits == locationCells.length) {
-            result = "kill";
-        }
-        System.out.println(result);
         return result;
     }
 }
